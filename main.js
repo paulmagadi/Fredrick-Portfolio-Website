542504ab773e908f34596e2650938a091183dd18
SQCodes <paulsqmagadi@gmail.com>
2024-07-31 22:31:38 +0300

    modified:   images/admin_portal.png 	modified:   index.html 	modified:   main.css 	modified:   main.js 	modified:   responsive.css

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const contactsBtn = document.querySelector('.view-contacts-button');
 const contacts = document.querySelector('.profile-information-segment');
-const viewContactIcon = document.querySelector('.view-contact-icon');
-
+const lineBreak = document.querySelector('.line-break');
 
 contactsBtn.addEventListener('click', function() {
     contacts.classList.toggle('active');
 
+    if (contacts.classList.contains('active')) {
+        contactsBtn.innerHTML = '<p>Close Contacts &uparrow;</p>';
+        lineBreak.style.display = 'block';
+      } else {
+        contactsBtn.innerHTML = '<p>View Contacts &downarrow;</p> ';
+        lineBreak.style.display = 'none';
+      }
 })
+
+
